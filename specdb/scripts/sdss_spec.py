6dd8745fac9f42ff3b495973cf65dc83daf819a4
J. Xavier Prochaska <xavier@ucolick.org>
2017-08-27 07:27:50 -0700

    sdss_method for igmspec

--- specdb/scripts/sdss_spec.py
+++ specdb/scripts/sdss_spec.py
@@ -10,10 +10,10 @@ def parser(options=None):
 
     import argparse
 
-    parser = argparse.ArgumentParser(description='specdb_sdss script v0.1')
+    parser = argparse.ArgumentParser(description='specdb_sdss script v0.2')
     parser.add_argument("plate", type=int, help="Plate")
     parser.add_argument("fiberid", type=int, help="FiberID")
-    parser.add_argument("dbase", type=str, help="Database [igmspec,all]")
+    parser.add_argument("--dbase", default='igmspec', type=str, help="Database [igmspec,all]")
     parser.add_argument("--survey", help="Name of Survey to use (BOSS_DR12 or SDSS_DR7)")
     parser.add_argument("--select", default=0, type=int, help="Index of spectrum to plot (when multiple exist)")
     parser.add_argument("-p", "--plot", default=False, action="store_true", help="Plot with lt_xspec")
@@ -37,35 +37,17 @@ def main(args, unit_test=False, **kwargs):
     from specdb import group_utils
 
     # init
+    if args.dbase not in ['igmspec']:
+        raise IOError("Not prepared for this database: {:s}".format(args.dbase))
     Specdb = load_db(args.dbase, **kwargs)
 
-    # Load meta table(s)
     if args.survey is None:
         surveys = ['BOSS_DR12', 'SDSS_DR7']
     else:
         surveys = [args.survey]
-    for kk,survey in enumerate(surveys):
-        meta = Specdb[survey].meta
-        if 'FIBERID' not in meta.keys():
-            meta.rename_column('FIBER','FIBERID')
-        if kk > 0:
-            mtbl = vstack([mtbl, meta], join_type='inner')
-        else:
-            mtbl = meta
 
-    # Find plate/fiber
-    imt = np.where((mtbl['PLATE'] == args.plate) & (mtbl['FIBERID'] == args.fiberid))[0]
-    if len(imt) == 0:
-        print("Plate and Fiber not found.  Try again")
-        return
-    else:
-        mt = imt[0]
-        scoord = SkyCoord(ra=mtbl['RA_GROUP'][mt], dec=mtbl['DEC_GROUP'][mt], unit='deg')
-
-    # Grab
-    print("Grabbing data for J{:s}{:s}".format(scoord.ra.to_string(unit=u.hour,sep='',pad=True),
-                                              scoord.dec.to_string(sep='',pad=True,alwayssign=True)))
-    spec, meta = Specdb.spectra_from_coord(scoord, groups=surveys)
+    # Load
+    spec, meta = Specdb.get_sdss(args.plate, args.fiberid, groups=surveys)
 
     # Outcome
     if len(meta) == 0:
