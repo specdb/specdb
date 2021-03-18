d9e99828ce63c27437c506839c8f9612e8bc50dc
profxj <xavier@ucolick.org>
2021-03-18 12:56:18 -0700

    deimos in progress

--- specdb/defs.py
+++ specdb/defs.py
@@ -220,6 +220,8 @@ def get_res_dicts():
                    '400/8500': 1232.,
                    '1200/7500': 2*1595.,
                    }
+    DEIMOS_Rdict = {'600ZD': 1607.,     # Assumes 1" slit
+                   }
     MOSFIRE_Rdict = {'H': 3660,  # Assumes 0.7" slit
                      }
     KCWI_Rdict = {'BL': 900,  # Assumes Large IFU slicer
